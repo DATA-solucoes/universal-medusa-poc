8b37b89cd6d7423f5a625989d7e29bfa7995b7aa
andre.castro <andre.castro@defensoria.mg.def.br>
2024-08-26 23:27:03 -0300

    Configuring shad-cn

--- universal-custom-admin/nuxt.config.ts
+++ universal-custom-admin/nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    componentDir: './components/ui'
+  }
 })
--- universal-custom-admin/pages/index.vue
+++ universal-custom-admin/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1 class="text-2xl text-slate-800">Home page</h1>
+  <Button>Click me</Button>
 </template>
