0ed2841eac271940235785320f510b9003511dee
andre.castro <andre.castro@defensoria.mg.def.br>
2024-08-26 22:19:10 -0300

    update cors

--- universal-medusa-app/medusa-config.js
+++ universal-medusa-app/medusa-config.js
@@ -23,10 +23,10 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "/http:\\/\\/localhost:700\\d+$/" // "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS ||  "http://localhost:7000,http://localhost:7001,http://localhost:3000" // "/http:\\/\\/localhost:700\\d+$/" // "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000,http://localhost:3000";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
